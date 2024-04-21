9f253599fe7d2fe3ab248f8a3fe8f072fbf7d525
米奇 <your_email@example.com>
2024-04-21 19:18:55 +0800

    这个是后台管理系统

---
+++ blog-admin/src/main/java/com/blog/admin/AdminApp.java
@@ -0,0 +1,13 @@
+package com.blog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class AdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class,args);
+    }
+}
